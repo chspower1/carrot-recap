68f0f92b5a89800297ba2719e1d3f9d7e41e50e6
chspower1 <chspower1@gmail.com>
2022-11-01 22:19:19 +0900

    style(enter) : tailwind 로그인폼

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await client.user.create({
+      data: {
+        email: "test22",
+        name: "chs",
+      },
+    });
+    res.json({ data: "CREATE" });
+  } catch (err) {
+    console.log(err);
+  }
+}
