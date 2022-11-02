25a07d6b10505871fd3c6364836d38f660b26dc4
chspower1 <chspower1@gmail.com>
2022-11-03 02:45:39 +0900

    9.2 / 11:05

--- docs/prisma.md
+++ docs/prisma.md
@@ -1,5 +1,18 @@
 # prisma 메모
 
 ## schema.prisma
+
 - model들을 데이터베이스에 push하고 SQL migration을 자동을 처리하기 위함
-- Database와 상호작용하기 위해 client를 생성하고 
+- Database와 상호작용하기 위해 client를 생성하고
+
+## upsert (update + insert)
+
+생성하거나 수정할 때 사용
+
+```ts
+await client.user.upsert({
+  where: {},
+  create: {},
+  update: {},
+});
+```
--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,8 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function getEnter(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { email, phone } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      ...payload,
+      name: "Anonymous",
+    },
+    update: {},
+  });
+  const token = await client.token.create({
+    data: {
+      payload: "12356",
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+  console.log(token);
+  console.log(user);
   return res.status(200).end();
 }
 export default withHandler("POST", getEnter);
