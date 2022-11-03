e1b9828491bb2fc5c3628e80a0448fcd50fd86b1
chspower1 <chspower1@gmail.com>
2022-11-04 03:12:36 +0900

    feat(enter) : iron session 구현

---
+++ libs/server/withSession.ts
@@ -0,0 +1,16 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+const cookieOptions = {
+  cookieName: "carrotsession",
+  password: process.env.COOKIE_PASSWORD!,
+};
+export function withApiSession(fn: any) {
+  return withIronSessionApiRoute(fn, cookieOptions);
+}
